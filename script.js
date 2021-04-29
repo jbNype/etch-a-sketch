4bae408c0799270eea3de17faff1fa044c0a04c4
Justin Bauer <justinbauer.design@gmail.com>
2021-04-29 14:46:58 +0200

    New plan of attack

---
+++ script.js
@@ -0,0 +1,22 @@
+// create 16x16 grid
+    // create divs using createElement.
+    // appendChild divs to '.container' div
+    // allow divs to be created with inline-block
+    // set borders and width for new grid divs
+
+// When mouse hovers over grid squres do the following
+    // add event listners to divs for mouse hover
+    // change colour of hover grid  
+        // add new class with CSS attached, or new background-color
+
+// add button to reset grid
+// event listener for 'click'
+// run 'reset' function, clearing all hover effects by reseting CSS background color to white
+// prompt allows user to choose grid siz (eg. 64x64) -- ma grid size 100
+    // if user input is less than 100, then create new grid
+    // else ask for new number below 100
+
+// instead of black and white, ceate random RGB generator for grid
+    // (Math.random(Math.floor) * 10) for each specific number in RGB fo each hover
+    // after each pass the grid squre increases 10% to black
+        // 10 passes is equal to solid black
