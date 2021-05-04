b4c1bcb818c7fb0c1fd3d2a3b29ec3dd53f72bc0
Justin Bauer <justinbauer.design@gmail.com>
2021-05-04 16:18:00 +0200

    Project completed

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-const container = document.querySelector('.container');
+/******************************************************************************* 
+************************ The Odin Project: Etch-a-Sketch ***********************
+********************************************************************************
+***********************          by Justin Bauer          **********************
+***********************          GitHub: jbNype           **********************
+***********************     http://justinbauer.design     **********************
+********************************************************************************/
 
-// create 16x16 grid
+const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+createGrid(16);
+
 function createGrid(size) {
-    
     for(let row = 0; row < size; row++){
         for(let column = 0; column < size; column++) {
-            // create divs using createElement.
             let grid = document.createElement('div');
             grid.classList.add('grid');
-            // set height and width for new grid divs
-            grid.style.width = 500/size + 'px';
-            grid.style.height = 500/size + 'px';
-            // appendChild divs to '.container' div
+            grid.style.width = 600/size + 'px';
+            grid.style.height = 600/size + 'px';
             container.append(grid);
+        }    
+    }
+    randomColor();
+}
+
+button.addEventListener('click', () => {
+    let grid = document.querySelectorAll('.grid');
+    let gridArray = Array.from(grid);
+    for(let i = 0; i < gridArray.length; i++) {
+        gridArray[i].style.background = '#111111';
+    };
+    userGrid();
+});
+
+function userGrid() {
+    let newSize = prompt('Enter a new size?', '');
+
+    if (newSize != null) {
+        newSize = parseInt(newSize);
+        if (newSize < 1 || newSize > 64 || Number.isNaN(newSize)) {
+            alert('Enter a number between 4 and 64.');
+            userGrid(); 
+        } else {
+            clearGrid();
+            createGrid(newSize);
         }
-        
     }
 }
 
-// When mouse hovers over grid squres do the following
-    // add event listners to divs for mouse hover
-    // change colour of hover grid  
-        // add new class with CSS attached, or new background-color
+function clearGrid() {
+    let gridArray = Array.from(container.childNodes);
+    gridArray.forEach((element) => {
+        container.removeChild(element);
+    });
+}
 
-// add button to reset grid
-// event listener for 'click'
-// run 'reset' function, clearing all hover effects by reseting CSS background color to white
-// prompt allows user to choose grid siz (eg. 64x64) -- ma grid size 100
-    // if user input is less than 100, then create new grid
-    // else ask for new number below 100
-
-// instead of black and white, ceate random RGB generator for grid
-    // (Math.random(Math.floor) * 10) for each specific number in RGB fo each hover
-    // after each pass the grid squre increases 10% to black
-        // 10 passes is equal to solid black
+function randomColor() {
+    const squares = document.querySelectorAll('div');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', function (e) {
+            let randomR = (Math.floor(Math.random() * 256));
+            let randomG = (Math.floor(Math.random() * 256));
+            let randomB = (Math.floor(Math.random() * 256));
+            e.target.style.background = `rgb(${randomR},${randomG},${randomB})`;
+        });
+    });
+}
